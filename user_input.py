a99b67192a5a242e0e87a9267bc065fdaf22609c
MelvinKhakabo <melvinkhakabo@gmail.com>
2024-08-14 21:18:19 +0300

    input function by Melvin Khakabo

---
+++ user_input.py
@@ -0,0 +1,16 @@
+#name input function
+name = input("What is your name?")
+
+
+#age input function
+age = input("How old are you?")
+
+
+#location input function
+location = input("Where do you live?")
+
+#print the personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
+
+
+
